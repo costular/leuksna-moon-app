d91f354eda36c0ae0320991b1ebc164be8389305
Diego <costular@gmail.com>
2020-01-29 21:50:59 +0000

    Migrate to Gradle Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+}
+
+
+android {
+    compileSdkVersion(App.compileVersion)
+    buildToolsVersion(App.buildToolsVersion)
+    defaultConfig {
+        applicationId = "com.costular.leuksna_moon_phases"
+        minSdkVersion(App.minSdk)
+        targetSdkVersion(App.targetSdk)
+        versionCode = App.versionCode
+        versionName = App.versionName
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}")
+    implementation("androidx.appcompat:appcompat:1.0.2")
+    implementation("androidx.core:core-ktx:1.0.2")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test.ext:junit:1.1.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.1")
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,17 @@
+object App {
+
+    val compileVersion = 29
+    val buildToolsVersion = "29.0.2"
+    val minSdk = 21
+    val targetSdk = 29
+    val versionCode = 1
+    val versionName = "0.1"
+
+}
+
+
+object Versions {
+
+    val kotlin = "1.3.50"
+
+}
